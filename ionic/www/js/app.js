dd29f5d82bfbeb2590930d93f3cf7084b8cdd096
付大宝 <furezen@me.com>
2016-07-16 15:09:20 +0800

    Add ionic demo

---
+++ ionic/www/js/app.js
@@ -0,0 +1,66 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','wechat.routes'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+}).controller('Gua', function($scope,$ionicModal){
+  $scope.tasks=[
+  {title:"人生经验"},
+  {title:"https://www.youtube.com"},
+  {title:"西方国家"},
+  {title:"https://www.youku.com"}
+  ];
+//载入并创建模型
+$ionicModal.fromTemplateUrl('new-task.html',function(modal){
+  $scope.taskModal = modal;
+},{
+  scope:$scope,
+  animation:'slide-in-up'
+});
+//表单提交时调用
+$scope.createTask = function(task){
+  $scope.task.push({
+    title:tatle.title
+  });
+  $scope.taskModal.hide();
+  task.title="";
+  
+};
+//打开新增模型
+$scope.newTask = function(){
+  $scope.taskModal.show();
+};
+//关闭新模型
+$scope.closeNewTask = function(){
+  $scope.taskModal.hide();
+};
+$scope.task={title:""};
+$scope.fMessage = [
+	{name:"李卉",img:'0.JPG',fnew:'我还差你多少东西？'}
+]
+  
+}).config(['$ionicConfigProvider', function($ionicConfigProvider) {
+
+    $ionicConfigProvider.tabs.position('bottom'); // other values: top
+
+}]).controller('messageCtrl',function($scope){
+	$scope.task={title:"微信"}
+})
---
+++ ionic/www/js/routes.js
@@ -0,0 +1,48 @@
+//声明模块，在app.js中注入
+angular.module('wechat.routes', []).config(function($stateProvider,$urlRouterProvider) {
+	//默认是tabs.messages
+	$urlRouterProvider.otherwise("/tab/message");
+	$stateProvider
+	//如果tab状态被激活，加载tab.html模版
+	.state('tab',{
+		url:'/tab',
+		abstract:true,
+		templateUrl:"templates/tabs.html",
+		
+	})
+	.state('tab.message',{
+		url:'/message',
+		views:{
+		'tab-message':{
+		templateUrl:"templates/tab-message.html",
+		controller:"messageCtrl"
+		}
+	}
+	})
+	.state('tab.friends',{
+		url:'/friends',
+		views:{
+		'tab-friends':{
+		templateUrl:"templates/tab-friends.html"
+		}
+	}
+	})
+	.state('tab.find',{
+		url:'/find',
+		views:{
+			'tab-find':{
+				templateUrl:'templates/tab-find.html'
+			}
+		}
+	})
+	.state('tab.setting', {
+            url: '/setting',
+            views: {
+                'tab-setting': {
+                    templateUrl: 'templates/tab-setting.html',
+                    //controller: "settingCtrl"
+                }
+            }
+        })
+	
+})
